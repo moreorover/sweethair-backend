13ee69abbefc79d9638d93ad977e781b78d6dec8
Martin <mselvenis@gmail.com>
2021-09-26 18:15:01 +0100

    Transaction service

---
+++ src/services/transaction.service.ts
@@ -0,0 +1,4 @@
+import { Transaction } from './../entity/hair/Transaction';
+import { AbstractService } from './abstract.service';
+
+export class TransactionService extends AbstractService<Transaction> {}
