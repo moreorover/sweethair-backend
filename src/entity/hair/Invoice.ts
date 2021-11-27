e48b7123ec381fdc25ea4e6e24e05ac39a83c106
Martin <mselvenis@gmail.com>
2021-11-27 20:55:38 +0000

    invoice entity

---
+++ src/entity/hair/Invoice.ts
@@ -0,0 +1,28 @@
+import { Transaction } from './Transaction';
+import { Entity, Column, PrimaryGeneratedColumn, OneToMany } from 'typeorm';
+import { BaseEntity } from '../BaseEntity';
+import { Expose } from 'class-transformer';
+
+@Entity()
+@Expose()
+export class Invoice extends BaseEntity {
+    @PrimaryGeneratedColumn('uuid')
+    id: string;
+
+    @Column('float', { precision: 11, scale: 2 })
+    total: number;
+
+    @Column({ default: false, type: 'boolean' })
+    received: boolean;
+
+    @Column({ default: false, type: 'boolean' })
+    paid: boolean;
+
+    @Column({ default: null })
+    scheduledAt: Date;
+
+    @OneToMany(() => Transaction, (transaction) => transaction.invoice, {
+        cascade: true
+    })
+    transactions: Transaction[];
+}
