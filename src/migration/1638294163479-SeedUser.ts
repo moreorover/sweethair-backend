3134798f4c603cd3af64b6ccf6da300bbb029aae
Martin <mselvenis@gmail.com>
2021-11-30 17:48:55 +0000

    seed admin user migration

---
+++ src/migration/1638294163479-SeedUser.ts
@@ -0,0 +1,21 @@
+import { MigrationInterface, QueryRunner } from 'typeorm';
+import { User } from '../entity/User';
+import { UserService } from '../services/user.service';
+
+export class SeedUser1638294163479 implements MigrationInterface {
+    public async up(queryRunner: QueryRunner): Promise<void> {
+        const admin = {
+            firstName: 'Admin',
+            lastName: 'Sweet',
+            email: 'test@gmail.com',
+            password: 'adminadmin',
+            role_id: 1
+        };
+
+        const service: UserService = new UserService(User);
+
+        await service.create(admin);
+    }
+
+    public async down(queryRunner: QueryRunner): Promise<void> {}
+}
