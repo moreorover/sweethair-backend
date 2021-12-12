aef2cdd36019dfbeaaa78002dcda5a57f90e7aec
Martin <mselvenis@gmail.com>
2021-12-12 23:25:14 +0000

    linting

--- src/controllers/appointment.controller.ts
+++ src/controllers/appointment.controller.ts
@@ -1,4 +1,3 @@
-import { Customer } from './../entity/hair/Customer';
 import { Appointment } from './../entity/hair/Appointment';
 import { plainToClass } from 'class-transformer';
 import { AppointmentService } from '../services/appointment.service';
@@ -6,68 +5,94 @@ import { PaginateDto } from './dtos/common/paginate.dto';
 import { Request, Response } from 'express';
 import { AppointmentCreateDto } from './dtos/appointment/appointment-create.dto';
 import { AppointmentUpdateDto } from './dtos/appointment/appointment-update.dto';
+import { AppointmentSaveCustomersDto } from './dtos/appointment/appointment-save-customers.dto';
 
 export const all = async (req: Request, res: Response) => {
-    const service: AppointmentService = new AppointmentService(Appointment);
-    const results = await service.all({ relations: ['customers', 'transactions', 'items'], order: { scheduledAt: 'ASC' } });
-    return res.json(results);
+  const service: AppointmentService = new AppointmentService(Appointment);
+  const results = await service.all({
+    relations: ['customers', 'transactions', 'items'],
+    order: { scheduledAt: 'ASC' },
+  });
+  return res.json(results);
 };
 
 export const paginate = async (req: Request, res: Response) => {
-    const service: AppointmentService = new AppointmentService(Appointment);
-    const { page }: PaginateDto = plainToClass(PaginateDto, req.body);
-    const result = await service.paginate(page, { relations: ['customers', 'transactions', 'items'], order: { scheduledAt: 'ASC' } });
-    return res.send(result);
+  const service: AppointmentService = new AppointmentService(Appointment);
+  const { page }: PaginateDto = plainToClass(PaginateDto, req.body);
+  const result = await service.paginate(page, {
+    relations: ['customers', 'transactions', 'items'],
+    order: { scheduledAt: 'ASC' },
+  });
+  return res.send(result);
 };
 
 export const findById = async (req: Request, res: Response) => {
-    const service: AppointmentService = new AppointmentService(Appointment);
-    const results = await service.findOne({ id: parseInt(req.params.id) });
-    return res.send(results);
+  const service: AppointmentService = new AppointmentService(Appointment);
+  const results = await service.findOne({ id: parseInt(req.params.id) });
+  return res.send(results);
 };
 
 export const create = async (req: Request, res: Response) => {
-    const service: AppointmentService = new AppointmentService(Appointment);
-    const body: AppointmentCreateDto = plainToClass(AppointmentCreateDto, req.body);
-    const savedCustomer: Promise<Appointment> = await service.create(body);
-    return res.send(savedCustomer);
+  const service: AppointmentService = new AppointmentService(Appointment);
+  const body: AppointmentCreateDto = plainToClass(
+    AppointmentCreateDto,
+    req.body
+  );
+  const savedCustomer: Promise<Appointment> = await service.create(body);
+  return res.send(savedCustomer);
 };
 
 export const update = async (req: Request, res: Response) => {
-    const service: AppointmentService = new AppointmentService(Appointment);
-    const body: AppointmentUpdateDto = plainToClass(AppointmentUpdateDto, req.body, { strategy: 'excludeAll' });
-    await service.update(parseInt(req.params.id), body);
-    const savedCustomer = await service.findOne({ id: parseInt(req.params.id) }, { relations: ['customers', 'transactions', 'items'] });
-    return res.send(savedCustomer);
+  const service: AppointmentService = new AppointmentService(Appointment);
+  const body: AppointmentUpdateDto = plainToClass(
+    AppointmentUpdateDto,
+    req.body,
+    { strategy: 'excludeAll' }
+  );
+  await service.update(parseInt(req.params.id), body);
+  const savedCustomer = await service.findOne(
+    { id: parseInt(req.params.id) },
+    { relations: ['customers', 'transactions', 'items'] }
+  );
+  return res.send(savedCustomer);
 };
 
 export const deleteById = async (req: Request, res: Response) => {
-    const service: AppointmentService = new AppointmentService(Appointment);
-    return res.send(await service.delete(parseInt(req.params.id)));
+  const service: AppointmentService = new AppointmentService(Appointment);
+  return res.send(await service.delete(parseInt(req.params.id)));
 };
 
 export const fetchTransactions = async (req: Request, res: Response) => {
-    const service: AppointmentService = new AppointmentService(Appointment);
-    const { transactions } = await service.findOne({ id: parseInt(req.params.id) }, { relations: ['transactions', 'transactions.customer'] });
-    const t = transactions.map((t) => {
-        return { ...t, customer: { id: t.customer?.id || null } };
-    });
-    return res.send(t);
+  const service: AppointmentService = new AppointmentService(Appointment);
+  const { transactions } = await service.findOne(
+    { id: parseInt(req.params.id) },
+    { relations: ['transactions', 'transactions.customer'] }
+  );
+  const t = transactions.map((t) => {
+    return { ...t, customer: { id: t.customer?.id || null } };
+  });
+  return res.send(t);
 };
 
 export const fetchCustomers = async (req: Request, res: Response) => {
-    const service: AppointmentService = new AppointmentService(Appointment);
-    const { customers } = await service.findOne({ id: parseInt(req.params.id) }, { relations: ['customers'] });
-    return res.send(customers);
+  const service: AppointmentService = new AppointmentService(Appointment);
+  const { customers } = await service.findOne(
+    { id: parseInt(req.params.id) },
+    { relations: ['customers'] }
+  );
+  return res.send(customers);
 };
 
 export const fetchItems = async (req: Request, res: Response) => {
-    const service: AppointmentService = new AppointmentService(Appointment);
-    const { items } = await service.findOne({ id: parseInt(req.params.id) }, { relations: ['items', 'items.customer'] });
-    const i = items.map((i) => {
-        return { ...i, customer: { id: i.customer?.id || null } };
-    });
-    return res.send(i);
+  const service: AppointmentService = new AppointmentService(Appointment);
+  const { items } = await service.findOne(
+    { id: parseInt(req.params.id) },
+    { relations: ['items', 'items.customer'] }
+  );
+  const i = items.map((i) => {
+    return { ...i, customer: { id: i.customer?.id || null } };
+  });
+  return res.send(i);
 };
 
 export const addCustomers = async (req: Request, res: Response) => {
