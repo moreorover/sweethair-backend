a88550575635d4ada0c5e0b6fca2ddafeffb89df
Martin <mselvenis@gmail.com>
2021-12-12 23:24:13 +0000

    customers base route added

--- src/routes/customer.routes.ts
+++ src/routes/customer.routes.ts
@@ -3,13 +3,14 @@ import { PaginateDto } from '../controllers/dtos/common/paginate.dto';
 import { CustomerCreateDto } from '../controllers/dtos/customer/customer-create.dto';
 import { CustomerUpdateDto } from '../controllers/dtos/customer/customer-update.dto';
 import { authenticateToken } from '../middleware/requestAuthenticated';
-import { all, paginate, create, findById, update, deleteById } from '../controllers/customer.controller';
+import { all, paginate, create, findById, update, deleteById, customersBase } from '../controllers/customer.controller';
 import { validateDto } from '../middleware/validateBodyToDto';
 
 const router = Router();
 
 router.get('', authenticateToken, all);
 router.post('/paginate', authenticateToken, validateDto(PaginateDto), paginate);
+router.get('/base', authenticateToken, customersBase);
 router.post('', authenticateToken, validateDto(CustomerCreateDto), create);
 router.get('/:id', authenticateToken, findById);
 router.patch('/:id', authenticateToken, validateDto(CustomerUpdateDto), update);
