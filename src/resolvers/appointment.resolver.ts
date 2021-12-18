ce269859affa8c94eff241818a476b424d954d08
Martin <mselvenis@gmail.com>
2021-12-18 23:34:22 +0000

    appointment resolver

--- src/resolvers/appointment.resolver.ts
+++ src/resolvers/appointment.resolver.ts
@@ -1,54 +1,93 @@
 import {
   AppointmentCreate,
-  AppointmentCustomersUpdate,
+  AppointmentUpdate,
 } from './types/appointment.types';
 import { Appointment } from './../entity/hair/Appointment';
 import { AppointmentService } from './../services/appointment.service';
-import { Resolver, Query, Arg, Mutation } from 'type-graphql';
+import { Resolver, Query, Arg, Mutation, Int } from 'type-graphql';
 import { getConnection } from 'typeorm';
 
 @Resolver(Appointment)
 export class AppointmentResolver {
   @Query(() => [Appointment])
   // @UseMiddleware(isAuth)
-  appointments() {
+  appointments(): Promise<Appointment[]> {
     const service: AppointmentService = new AppointmentService(Appointment);
     return service.all();
   }
 
-  @Query(() => Appointment)
-  async appointment(@Arg('appointmentId') appointmentId: number) {
+  @Query(() => Appointment, { nullable: true })
+  async appointment(
+    @Arg('appointmentId', (type) => Int) appointmentId: number
+  ): Promise<Appointment | undefined> {
     const appointment = await Appointment.findOne(appointmentId);
     return appointment;
   }
 
   @Mutation(() => Appointment)
   async createAppointment(
-    @Arg('input') input: AppointmentCreate
+    @Arg('appointment') appointment: AppointmentCreate
   ): Promise<Appointment> {
-    return Appointment.create(input).save();
+    return Appointment.create(appointment).save();
   }
 
   @Mutation(() => Appointment)
   async updateAppointment(
-    @Arg('appointmentId') appointmentId: number,
-    @Arg('scheduledAt') scheduledAt: Date,
-    @Arg('title') title: string
+    @Arg('appointment') appointment: AppointmentUpdate
   ): Promise<Appointment> {
     const result = await getConnection()
       .getRepository(Appointment)
-      .save({ id: appointmentId, title, scheduledAt });
+      .save(appointment);
     return result;
   }
 
   @Mutation(() => Appointment)
-  async updateAppointmentCustomers(
+  async addCustomerToAppointment(
     @Arg('appointmentId') appointmentId: number,
-    @Arg('customers') customers: AppointmentCustomersUpdate
+    @Arg('customerId') customerId: number
   ): Promise<Appointment> {
+    await getConnection()
+      .createQueryBuilder()
+      .relation(Appointment, 'customers')
+      .of(appointmentId)
+      .add(customerId);
+
     const result = await getConnection()
       .getRepository(Appointment)
-      .save({ id: appointmentId });
+      .createQueryBuilder('appointment')
+      .where('appointment.id = :id', { id: appointmentId })
+      .getOne();
+    return result;
+  }
+
+  @Mutation(() => Appointment)
+  async removeCustomerToAppointment(
+    @Arg('appointmentId') appointmentId: number,
+    @Arg('customerId') customerId: number
+  ): Promise<Appointment> {
+    const appointment: Appointment = await getConnection()
+      .getRepository(Appointment)
+      .findOne(appointmentId, { relations: ['items', 'transactions'] });
+
+    const canRemoveCustomer: boolean =
+      appointment.items.filter((i) => i.customerId === customerId).length ==
+        0 &&
+      appointment.transactions.filter((i) => i.customerId === customerId)
+        .length == 0;
+
+    if (canRemoveCustomer) {
+      await getConnection()
+        .createQueryBuilder()
+        .relation(Appointment, 'customers')
+        .of(appointmentId)
+        .remove(customerId);
+    }
+
+    const result = await getConnection()
+      .getRepository(Appointment)
+      .createQueryBuilder('appointment')
+      .where('appointment.id = :id', { id: appointmentId })
+      .getOne();
     return result;
   }
 }
