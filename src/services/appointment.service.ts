2419805b7f96edee0294b7f8e1a5bf94185730d1
Martin <mselvenis@gmail.com>
2022-01-07 18:25:01 +0000

    generics updated

--- src/services/appointment.service.ts
+++ src/services/appointment.service.ts
@@ -1,10 +1,8 @@
-import { AppointmentUpdateDto } from './../controllers/dtos/appointment/appointment-update.dto';
 import { AppointmentCreateDto } from './../controllers/dtos/appointment/appointment-create.dto';
 import { Appointment } from './../entity/hair/Appointment';
 import { AbstractService } from './abstract.service';
 
 export class AppointmentService extends AbstractService<
   Appointment,
-  AppointmentCreateDto,
-  AppointmentUpdateDto
+  AppointmentCreateDto
 > {}
--- src/services/role.service.ts
+++ src/services/role.service.ts
@@ -1,10 +1,5 @@
-import { RoleUpdateDto } from './../controllers/dtos/role/role-update.dto';
 import { RoleCreateDto } from './../controllers/dtos/role/role-create.dto';
 import { Role } from '../entity/Role';
 import { AbstractService } from './abstract.service';
 
-export class RoleService extends AbstractService<
-  Role,
-  RoleCreateDto,
-  RoleUpdateDto
-> {}
+export class RoleService extends AbstractService<Role, RoleCreateDto> {}
