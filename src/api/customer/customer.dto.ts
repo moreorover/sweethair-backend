d537aa2b2cce19853fb7125bf0712e10c705c092
moreorover <mselvenis@gmail.com>
2022-04-23 13:01:29 +0100

    object validation adjustments

--- src/api/customer/customer.dto.ts
+++ src/api/customer/customer.dto.ts
@@ -1,44 +1,35 @@
-import { IsEmail, IsNotEmpty, IsOptional } from 'class-validator';
+import { IsNotEmpty, IsOptional } from 'class-validator';
 
 export class CustomerCreateDto {
   @IsNotEmpty()
   fullName: string;
 
   @IsOptional()
-  @IsNotEmpty()
   location: string;
 
   @IsOptional()
-  @IsNotEmpty()
   about: string;
 
   @IsOptional()
-  @IsEmail()
   email: string;
 
   @IsOptional()
-  @IsNotEmpty()
   instagram: string;
 }
 
 export class CustomerUpdateDto {
   @IsOptional()
-  @IsNotEmpty()
   fullName: string;
 
   @IsOptional()
-  @IsNotEmpty()
   location: string;
 
   @IsOptional()
-  @IsNotEmpty()
   about: string;
 
   @IsOptional()
-  @IsEmail()
   email: string;
 
   @IsOptional()
-  @IsNotEmpty()
   instagram: string;
 }
