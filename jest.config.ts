6253793677bbb1118337d164abde645299d35ee9
Martin <mselvenis@gmail.com>
2022-10-05 18:45:05 +0100

    jest settings

--- jest.config.ts
+++ jest.config.ts
@@ -1,6 +1,11 @@
-module.exports = {
+import type { Config } from 'jest';
+
+const config: Config = {
   clearMocks: true,
   preset: 'ts-jest',
   testEnvironment: 'node',
   setupFilesAfterEnv: ['<rootDir>/tests/prismaMock.ts'],
+  collectCoverage: true,
 };
+
+export default config;
