2c98ed24504f7fa9d167bfdc8b73b71213b19e4b
Martin <mselvenis@gmail.com>
2022-10-16 15:29:10 +0100

    user controller

---
+++ src/main/java/dev/martin/sweethair/entity/User.java
@@ -0,0 +1,43 @@
+package dev.martin.sweethair.entity;
+
+import javax.persistence.*;
+
+@Entity
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    @Column(name = "id", nullable = false)
+    private Long id;
+
+    @Column(name = "full_name", nullable = false, unique = true)
+    private String fullName;
+
+    @Column(name = "email_address", nullable = false, unique = true)
+    private String emailAddress;
+
+    public String getEmailAddress() {
+        return emailAddress;
+    }
+
+    public void setEmailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+}
